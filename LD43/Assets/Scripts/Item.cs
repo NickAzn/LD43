cac71987ca1d106f55b64d5af512e6aec328c7c8
Sean Newman <sean.m.newman@comcast.net>
2018-11-30 21:08:10 -0800

    Added variables and Increase method to Item script

--- LD43/Assets/Scripts/Item.cs
+++ LD43/Assets/Scripts/Item.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Item : MonoBehaviour {
 
+    public float farming;
+    public float sex;
+    public float building;
+    public float hunting;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        farming = Random.Range(0f, 3f);
+        sex = Random.Range(0f, 3f);
+        building = Random.Range(0f, 3f);
+        hunting = Random.Range(0f, 3f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Increase() {
+
+    }
 }
