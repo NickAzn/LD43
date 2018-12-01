e6fcc68861cfc3a38e9bd6067666c0913dd6fb79
Sean Newman <sean.m.newman@comcast.net>
2018-11-30 20:42:00 -0800

    Added variables and gender logic to Person Script

--- LD43/Assets/Scripts/Person.cs
+++ LD43/Assets/Scripts/Person.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Person : MonoBehaviour {
 
+    private float rndNumber;
+    
+    public string gender;
+    public int age;
+    public bool homeless;
+    public int job;
+    public bool pregnant;
+    
 	// Use this for initialization
 	void Start () {
-		
+        rndNumber = Random.Range(1f, 2f);
+
+        if(rndNumber == 1){
+            gender = "male";
+        }
+        else {
+            gender = "female";
+        }
+
+        age = 0;
+        homeless = true;
+        job = 0;
 	}
 	
 	// Update is called once per frame
