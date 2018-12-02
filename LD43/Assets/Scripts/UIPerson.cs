f4668e6900d93427cbf070f880698ab9d26c493d
NickAzn <nicholaslam16@gmail.com>
2018-12-01 19:26:53 -0800

    Created basic UIPerson

---
+++ LD43/Assets/Scripts/UIPerson.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+// Class to display UI for a person
+public class UIPerson : MonoBehaviour {
+
+    public Person person;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI skillsText;
+
+	public void UpdateDisplay() {
+        if (person != null) {
+            nameText.text = person.name;
+            skillsText.text = "Fertility: " + person.GetFertilSkill()
+                               + "\nFarming: " + person.GetFarmSkill()
+                               + "\nBuilding: " + person.GetBuildSkill()
+                               + "\nHunting: " + person.GetHuntSkill();
+        }
+    }
+}
