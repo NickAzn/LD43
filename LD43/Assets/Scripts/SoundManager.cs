0d56dc146ccae7896398e5fbf947c05809a616fa
NickAzn <nicholaslam16@gmail.com>
2018-12-01 17:03:08 -0800

    Changed audio import settings, created basic sound manager

---
+++ LD43/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance = null;
+
+    public AudioSource music;
+    public AudioSource sfx;
+
+    // Use this for initialization
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySfx(AudioClip clip) {
+        sfx.PlayOneShot(clip);
+    }
+
+    public void PlayMusic(AudioClip clip) {
+        music.clip = clip;
+        music.Play();
+    }
+}
